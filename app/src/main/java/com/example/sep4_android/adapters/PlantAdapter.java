0b2686c07a18549c4afa502ef8360e6b6bb106e6
Estera <dumi_estera@yahoo.com>
2021-05-12 12:12:19 +0200

    RecyclerView stuff

--- app/src/main/java/com/example/sep4_android/adapters/PlantAdapter.java
+++ app/src/main/java/com/example/sep4_android/adapters/PlantAdapter.java
@@ -1,4 +1,44 @@
 package com.example.sep4_android.adapters;
 
-public class PlantAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sep4_android.R;
+
+public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.ViewHolder> {
+
+    private View view;
+
+    public PlantAdapter(View view) {
+        this.view = view;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.recycleviewdesign,parent,false);
+        return new PlantAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
