b02f6743ab83ac0013796882652283342e59d643
Estera <dumi_estera@yahoo.com>
2021-05-12 20:00:29 +0200

    RecyclerView stuff 2

--- app/src/main/java/com/example/sep4_android/adapters/PlantAdapter.java
+++ app/src/main/java/com/example/sep4_android/adapters/PlantAdapter.java
@@ -1,20 +1,28 @@
 package com.example.sep4_android.adapters;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.sep4_android.R;
+import com.example.sep4_android.models.Plant;
+
+import java.util.ArrayList;
 
 public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.ViewHolder> {
 
-    private View view;
+    private ArrayList<Plant> plantArrayList;
 
-    public PlantAdapter(View view) {
-        this.view = view;
+    public PlantAdapter(ArrayList<Plant> plantArrayList) {
+        this.plantArrayList=plantArrayList;
+
     }
 
     @NonNull
@@ -27,7 +35,11 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+       holder.button.setOnClickListener(v->{
+           Bundle bundle = new Bundle();
+           bundle.putInt("plantId", plantArrayList.get(position).getPlant_ID());
+           Navigation.findNavController(holder.itemView).navigate(R.id.action_gardenListFragment_to_addPlantFragment,bundle);
+       });
     }
 
 
@@ -37,8 +49,15 @@ public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name;
+        Button button;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.plantName);
+            button = itemView.findViewById(R.id.buttonOpen);
         }
+
     }
 }
--- app/src/main/java/com/example/sep4_android/views/mainapp/GardenListFragment.java
+++ app/src/main/java/com/example/sep4_android/views/mainapp/GardenListFragment.java
@@ -3,21 +3,31 @@ package com.example.sep4_android.views.mainapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.sep4_android.R;
+import com.example.sep4_android.adapters.PlantAdapter;
+import com.example.sep4_android.models.Plant;
+
+import java.util.ArrayList;
 
 
-public class GardenListFragment extends Fragment {
+public class GardenListFragment extends Fragment  {
 
 private RecyclerView recyclerView;
 private View view;
+private PlantAdapter plantAdapter;
+private ArrayList<Plant> plantArrayList;
+private Button button;
+
 
 
     @Override
@@ -27,6 +37,15 @@ private View view;
         recyclerView = view.findViewById(R.id.recycler);
         recyclerView.hasFixedSize();
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(),LinearLayoutManager.HORIZONTAL,false));
+
+//        ArrayList<Plant> plantArrayList = new ArrayList<>();
+//        plantAdapter = new PlantAdapter(plantArrayList,this);
+        recyclerView.setAdapter(plantAdapter);
         return view;
     }
+//
+//    @Override
+//    public void onListItemClick(int plantId) {
+//        button.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_gardenListFragment_to_addPlantFragment));
+//    }
 }
